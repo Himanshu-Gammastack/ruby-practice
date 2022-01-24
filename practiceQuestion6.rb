d724b137a40f9c1b2b27330873bdbb1f451dd383
Himanshu Gupta <hgupta@grepruby.io>
2022-01-24 19:49:20 +0530

    final commit ruby basic 1

---
+++ practiceQuestion6.rb
@@ -0,0 +1,8 @@
+arr1=[1,2,3,"himanshu"]
+arr2=arr1.map { |e|
+  if e.is_a? Numeric
+    e = (e.to_i*2).to_s
+  else e.to_s
+  end
+}
+puts arr2
---
+++ practiceQuestion7.rb
@@ -0,0 +1,16 @@
+File.open("friends.txt", "r") do |file|
+  #read full file
+  #puts file.read()
+  #to read single line
+  #puts file.readline()
+  # to read secondline file.readlines()[2]
+  # line by line read
+  for line in file.readlines()
+    puts line
+  end
+end
+file =File.open("friends.txt", "a")
+file.write("krishna raghuwanshi \n")
+file.close()
+file =File.open("friends.txt", "r")
+puts file.read()
