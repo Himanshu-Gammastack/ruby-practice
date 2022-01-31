638f692b72692a4cf4799145b6111b4437e65ca4
Himanshu Gupta <hgupta@grepruby.io>
2022-01-31 17:50:55 +0530

    corrected

--- calculate_tax.rb
+++ calculate_tax.rb
@@ -1,24 +1,21 @@
 class CalculateTax
-  def initialize(items)
+  def initialize(products)
     @products = products
     @total_amount = 0
     @total_tax = 0.0
   end
   def calculate
-    tax_free_products = ["book", "chocolate", "headache", "chocolates"]
+    tax_free_products = %w(book, chocolate, headache, chocolates)
     @products.each do |product|
        product_tax = 0.0
-      if product[:name].include? "imported"
-         product_tax += 0.05
-      end
+      if product[:name].include? "imported" then product_tax += 0.05 end
       if (product[:name].split.intersection(tax_free_products)).empty?
-        product_tax += 0.1
-      end
+        then  product_tax += 0.1 end
       @total_amount += product[:price] + product[:price] * product_tax
       @total_tax += product[:price] * product_tax
-      puts product[:quantity].to_s + " " + product[:name] + " : " + (product[:price] + product[:price] * product_tax).to_s
+      puts "#{product[:quantity].to_s}  #{product[:name]}  #{(product[:price] + product[:price] * product_tax).to_s}"
     end
-    puts ("Total Tax: " + @total_tax.to_s)
-    puts ("Total Ammount: " + @total_amount.to_s)
+    puts ("Total Tax:   #{@total_tax.to_s}")
+    puts ("Total Ammount:   #{@total_amount.to_s}")
   end
 end
