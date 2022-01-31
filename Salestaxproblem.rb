95b0a3a9c67227e9873ed7628d289312db39b9f8
Himanshu Gupta <hgupta@grepruby.io>
2022-01-31 12:02:07 +0530

    update filename

--- Salestaxproblem.rb
+++
@@ -1,7 +0,0 @@
-require './Inputuser.rb'
-require './Inputprocessor.rb'
-require './Calculatetax.rb'
-
-products = TakeInput.new.input
-product_hash_array =  InputProcessor.new(products).process
-CalculateTax.new(product_hash_array).calculate
